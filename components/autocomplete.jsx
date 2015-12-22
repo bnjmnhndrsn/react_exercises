68efbfa8e4cafa81fc9b933ceb7aa69176a5ce36
Ben Henderson <ben@nestio.com>
2015-12-22 18:13:27 -0500

    autocomplete widget

--- components/autocomplete.jsx
+++ components/autocomplete.jsx
@@ -1,12 +1,44 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
 
-var MyComponent = React.createClass({
-  render: function () {
-    return(
-      <div>Hello World</div>
-    );
-  }
+var Autocomplete = React.createClass({
+    getInitialState: function(){
+        return {
+            showList: true,
+            query: ''
+        }
+    },
+    _getFilteredData: function(){
+        var query = this.state.query;
+        return this.props.data.filter(function(item){
+            return !!item.match(query);
+        });
+    },
+    openMenu: function(e){
+        this.setState({showList: true});
+    },
+    handleClick: function(e){
+        this.setState({
+            query: e.target.innerText,
+            showList: false
+        });
+    },
+    handleChange: function(e){
+        this.setState({query: e.target.value});
+    },
+    render: function () {
+        var results = this._getFilteredData().map(function(datum, i){
+            return <li onClick={this.handleClick} key={i}>{datum}</li>;
+        }.bind(this));
+        
+        var menu = this.state.showList ? <ul>{results}</ul> : null;
+        
+        return (
+            <div>
+                <input value={this.state.query} type="text" onChange={this.handleChange} onClick={this.openMenu}/>
+                {menu}
+            </div>
+        );
+    }
 });
 
-module.exports = MyComponent;
+module.exports = Autocomplete;
--- index.jsx
+++ index.jsx
@@ -1,6 +1,17 @@
-var MyComponent = require('.components/hello_world');
-var Autocomplete = require('.components/autocomplete');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var MyComponent = require('./components/hello_world.jsx');
+var Autocomplete = require('./components/autocomplete.jsx');
+
+var data = [
+    'apples',
+    'oranges',
+    'tree',
+    'pear',
+    'grapefruit',
+    'asadf'
+];
 
 document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(<MyComponent />, document.getElementById('main'));
+  ReactDOM.render(<Autocomplete data={data} />, document.getElementById('main'));
 });
